cb605de912779d85e50172678ffadf47019770d0
niravkpatel28 <nirav.patel@raweng.com>
2021-07-21 18:11:24 +0530

    render-cards task

---
+++ 08-render-cards/src/app.js
@@ -0,0 +1,3 @@
+// on click of the button loadTeam
+// create and load employee card with relevant mark up
+// these cards will be loaded in the div with id=team
---
+++ 08-render-cards/src/team.js
@@ -0,0 +1,32 @@
+const team = [
+  {
+    empid: "CS1",
+    name: "Aarti G.",
+    team: "CSE",
+    company: "Contenstack",
+  },
+  {
+    empid: "CS2",
+    name: "Priyanka D.",
+    team: "CSE",
+    company: "Contenstack",
+  },
+  {
+    empid: "CS3",
+    name: "Devang G.",
+    team: "CSE",
+    company: "Contenstack",
+  },
+  {
+    empid: "CS3",
+    name: "Tanu S.",
+    team: "CSE-Team 2 ",
+    company: "Contenstack",
+  },
+  {
+    empid: "CS4",
+    name: "Nirav G.",
+    team: "People/LND",
+    company: "Surfboard",
+  },
+];
